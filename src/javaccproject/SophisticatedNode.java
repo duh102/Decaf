618d1297ec6616c2ef224b6dcd1d4290641b25b3
amthorn <amthorn@ncsu.edu>
2014-12-02 21:44:40 -0500

    Update SophisticatedNode.java

--- src/javaccproject/SophisticatedNode.java
+++ src/javaccproject/SophisticatedNode.java
@@ -1,5 +1,10 @@
 package javaccproject;
 
+import javaccproject.codegen.Access;
+import javaccproject.codegen.ClassDesc;
+import javaccproject.codegen.MethodDesc;
+import javaccproject.tokens.Token;
+
 public class SophisticatedNode extends SimpleNode
 {
 
@@ -11,24 +16,62 @@ public class SophisticatedNode extends SimpleNode
         super(i);
     }
     
+    @Override
     public Object jjtAccept(Exp1Visitor visitor, Object data) {
-        visitor.visit(this, data);
-        if (children != null) {
-            for (int i = 0; i < children.length; ++i) {
-                children[i].jjtAccept(visitor, data);
+        if (visitor instanceof javaccproject.CheckSTVisitor) {
+            //if checking variables    
+            visitor.visit(this, data);
+            if(children != null){
+                for(int i= 0; i < children.length; i++){
+                    //check
+                    if(this.jjtGetValue() == null || (!(this.jjtGetValue() instanceof Access)
+                            && !(this.jjtGetValue() instanceof ClassDesc)
+                            && !(this.jjtGetValue() instanceof MethodDesc)
+                            && !(this.jjtGetValue() instanceof Token))){
+                        children[i].jjtAccept(visitor, data);
+                    }else{
+                        SymbolTable s = null;
+                        if(this.jjtGetValue() instanceof ClassDesc){
+                            s = (SymbolTable)(((ClassDesc)this.jjtGetValue()).token).myContext;
+                        }else if(this.jjtGetValue() instanceof MethodDesc){
+                            s = (SymbolTable)(((MethodDesc)this.jjtGetValue()).token).myContext;
+                        }else if(this.jjtGetValue() instanceof Token){                         
+                            s = (SymbolTable)((Token)this.jjtGetValue()).myContext;
+                        }else if(this.jjtGetValue() instanceof Access){         
+                            s = (SymbolTable)(((Access)this.jjtGetValue()).token).myContext;                            
+                        }
+                        if(s != null){
+                            leftToRightScope current = new leftToRightScope(s);     
+                            children[i].jjtAccept(visitor, current);
+                        }
+                    }
+                }
+            }
+            return data;
+        } else {
+            visitor.visit(this, data);
+            String tabLevelNew = (String) data + " ";
+            if (children != null) {
+                for (int i = 0; i < children.length; ++i) {
+                    children[i].jjtAccept(visitor, tabLevelNew);
+                }
             }
         }
         return data;
     }
     
+    @Override
     public String toString()
     {
-        if(jjtGetValue() != null)
+        if(jjtGetValue() != null && !(jjtGetValue() instanceof javaccproject.codegen.Operator) 
+                && !(jjtGetValue() instanceof javaccproject.codegen.Literal))
         {
-            return String.format("%s:%s", Exp1TreeConstants.jjtNodeName[id], jjtGetValue());
+            return String.format("%s:%s\n", Exp1TreeConstants.jjtNodeName[id], ((Token)jjtGetValue()).image());
         }
-        else return "";
+        //else return "";
+        else return String.format("%s:%s\n", Exp1TreeConstants.jjtNodeName[id], "");
     }
+    
     public String toString(boolean valueOnly)
     {
        return valueOnly? (jjtGetValue() != null? jjtGetValue().toString():"") : toString();
